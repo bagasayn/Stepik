bc4094bca08339962cdd9034f824ce587ca43077
bagasayn <bagasayn@gmail.com>
2020-08-06 11:36:24 +0300

    На вход подаются строки A и B, разделенные пробелом.Вывести строку A, заменяя все B на символ _ и количество И

--- src/Main.kt
+++ src/Main.kt
@@ -3,10 +3,13 @@ val scan = java.util.Scanner(System.`in`)
 fun main() {
 
     val rez = ReturnSolve()
-    println("${rez.A.replace(rez.B,"_",true)} ")
+    println("${rez.A.replace(rez.B,"_")} ${countSubstring(rez.A,rez.B)}")
 
 
 }
+
+fun countSubstring(s: String, sub: String): Int = s.split(sub).size - 1
+
 class ReturnSolve(){
     val A: String = scan.next()
     val B: String = scan.next()
