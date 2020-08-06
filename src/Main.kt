3a30bebe33726e8eb6c122f1aee331cc89708436
bagasayn <bagasayn@gmail.com>
2020-08-06 09:20:12 +0300

    Шаблон для решения задач

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+import kotlin.math.*
+
+val scan = java.util.Scanner(System.`in`)
+fun main() {
+
+    Rez(scan.nextInt(),scan.nextDouble())
+
+
+}
+class Rez(val A: Int, val B: Double){
+    init {
+        
+        }
+    }
+
