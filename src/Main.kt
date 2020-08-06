3ea745471361f7c4a21bbaf7c7bae6f08a3b7122
bagasayn <bagasayn@gmail.com>
2020-08-06 10:59:18 +0300

    На вход подаются строки A и B, разделенные пробелом.Вывести строку A, заменяя все B на символ _

--- src/Main.kt
+++ src/Main.kt
@@ -3,16 +3,14 @@ val scan = java.util.Scanner(System.`in`)
 fun main() {
 
     val rez = ReturnSolve()
-    println("${rez.A.capitalize()} ${rez.B.capitalize()} ${rez.C.capitalize()} ${rez.D.capitalize()} ${rez.E.capitalize()}")
+    println("${rez.A.replace(rez.B,"_",true)} ")
 
 
 }
 class ReturnSolve(){
     val A: String = scan.next()
     val B: String = scan.next()
-    val C: String = scan.next()
-    val D: String = scan.next()
-    val E: String = scan.next()
+
 
 }
 
