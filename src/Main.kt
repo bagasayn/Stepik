135b8fdadf1b296de473dd6afad182ed673ff23a
bagasayn <bagasayn@gmail.com>
2020-08-21 12:26:27 +0300

    Понятие классов и модификаторов

--- src/Main.kt
+++ src/Main.kt
@@ -2,22 +2,29 @@ import java.util.Scanner
 
 val scan = java.util.Scanner(System.`in`)
 fun main() {
-    val dog1 = Dog()
-    val dog2 = Dog("max")
-    val dog3 = Dog("Maxi","Dog")
+    val male = Male("Сергей", 25, 82.5, 182.6)
+    male.hello(true)
+    val female = Female("Полина", 22, 75.0, 175.5)
+    female.hello(false)
+}
 
+abstract class Person (var name:String, var age:Int, var weight:Double, var height:Double){
+
+    fun hello(arg:Boolean) {
+        val old = when{
+            age<5 -> "года"
+            else  -> "лет"
+        }
+        if(arg){
+            println("Здравствуйте меня зовут $name я родился $age $old назад сейчас мой вес $weight, рост $height")
+        }else{
+            println("Здравствуйте меня зовут $name я родилась $age $old назад сейчас мой вес $weight, рост $height")
+        }
+    }
+}
+class Male(name: String, age: Int, weight: Double, height: Double) : Person(name, age, weight, height){
 
 }
-class Dog(private val name:String= "-"){
+class Female(name: String, age: Int, weight: Double, height: Double) : Person(name, age, weight, height){
 
-    constructor(name:String, nextName:String) : this(name){
-        print("Имя собаки $name и кличка $nextName")
-    }
-    init{
-        println("Имя собаки $name")
-    }
 }
-//Имя собаки -
-//Имя собаки max
-//Имя собаки Maxi
-//Имя собаки Maxi и кличка Dog
